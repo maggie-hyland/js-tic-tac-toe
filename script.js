fdfdb7009159a0ca92aa8a200b7a4419c88c42fd
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-12-14 17:49:04 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,18 +1,91 @@
-const X_CLASS = 'x'
-const O_CLASS = 'o'
+const X_CLASS = "x";
+const O_CLASS = "o";
+const WIN_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const cellElements = document.querySelectorAll("[cell-data]");
-let oTurn
+const board = document.getElementById("board");
+const winMessageElement = document.getElementById("winMessage");
+const restartButton = document.getElementById("restartBtn");
+const winMessageTextElement = document.querySelectorAll(
+  "[data-win-message-text]"
+);
+let oTurn;
 
-cellElements.forEach((cell) => {
-  cell.addEventListener("click", handleClick, { once: true });
-});
+startGame();
+
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+  oTurn = false;
+  cellElements.forEach((cell) => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(O_CLASS);
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHover();
+  winMessageElement.classList.remove("show");
+}
 
 function handleClick(e) {
-  const cell = e.target
-  const
-  //place x or o
-  //check for win
-  //check for draw
-  //switch turns
+  const cell = e.target;
+  const currentClass = oTurn ? O_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    changeTurn();
+    setBoardHover();
+  }
+}
 
+function endGame(draw) {
+  if (draw) {
+    winMessageTextElement.innerHTML = "Draw!";
+  } else {
+    winMessageTextElement.innerHTML = `${oTurn ? "O's" : "X's"} Wins!`;
+  }
+  winMessageElement.classList.add("show");
+}
+
+function isDraw() {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+  });
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function changeTurn() {
+  oTurn = !oTurn;
+}
+
+function setBoardHover() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(O_CLASS);
+  if (oTurn) {
+    board.classList.add(O_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
+}
+
+function checkWin(currentClass) {
+  return WIN_COMBOS.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
